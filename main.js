3023933f2a1af59b831dc461ee7c9a62c74d003f
mariicampos18 <mariana.cristina.campos@escola.pr.gov.br>
2025-04-08 11:33:47 +0000

    continuando

---
+++ main.js
@@ -0,0 +1,41 @@
+//declaração de variáveis
+let nome="joão";//Variável que pode mudar
+const idade =;//Variável constante 
+var cidade ="São Paulo";//Método antigo de
+declarar variáveis
+
+// Operadores matemáticos 
+let a =10;
+let b =5;
+
+let soma =a+b;
+let subtracao =a-b;
+letmultiblicacao =a*b;
+let divisao =a%b;
+letpotencia =a**b;
+let incremento = ++a;
+let decremento = --var;
+
+//Operadores de confirmparação
+let igual = (a == b);
+let diferente =(a !=b);
+let maior =(a>b);
+let menor =(a<b);
+let maiorOuigual=(a>=b);
+let menorOuigual=(a<=b);
+
+//Operadores lógicos 
+let eLogico =(a>5&&b<10);
+let ouLogico=(a>5||b>10);
+let naoLogico=!(a>5);
+
+//operadores de atribuição
+let c=10;
+c+=5;//C=C+5
+C-=3;//C=C-3
+C*=2;//C=C*2
+C/=4;//C=C/4
+C%=3;//C=C%3
+C**=2;//C=C**2
+//OPERADORES DE CONCATENAÇÃO(união de textos)
+let mensagem ="Olá,meu nome é"
